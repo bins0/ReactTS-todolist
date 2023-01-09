30f0ab199f7f4553938c04e385ecdc06f0f63422
chaeeon <chaeeon@naver.com>
2023-01-09 22:08:25 +0900

    style.css

--- src/conponents/model.tsx
+++ src/conponents/model.tsx
@@ -1,8 +1,37 @@
-
+import { useReducer } from "react";
 export interface Todo {
     id: number;
     todo: string;
     isDone: boolean;
 }
 
+// type Actions=
+//    | {type:'add', payload:string}
+//    | {type:'remove', payload:number}
+//    | {type:'done', payload:number}
 
+// const TodoReducer =(state:Todo[],action:Actions) => {
+//     switch (action.type) {
+//         case "add":
+            
+//             return[
+//                 ...state,
+//                 {id:Date.now(),todo:action.payload,isDone: false},
+//             ];ss
+//         case "remove":
+//             return state.filter((todo)=>todo.id !== action.payload);
+//         case "done":
+//             return state.map((todo)=>
+//                 todo.id !== action.payload ? {...todo,isDone:!todo.isDone} : todo)        
+//     }
+// }
+
+
+
+
+// const ReducerExmple = () => {
+//     const [state, dispatch] = useReducer(TodoReducer, [])
+//     return (
+//         <div/>
+//     )
+// }
